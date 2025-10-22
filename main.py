a7e0169a1d5b091fa26ca6dc4583ec36010c859e
Mario García Fuentes <mariogarfu03@gmail.com>
2025-10-22 21:02:59 +0200

    Segundo commit de la prueba de FastAPI, nuevas funciones añadidas

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+def root():
+    return {"Hello world!"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id :int) -> str:
+    if item_id < len(items):
+        item = items[item_id]
+        return item
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+    
+@app.get("/items")
+def get_items():
+    return items
+    
